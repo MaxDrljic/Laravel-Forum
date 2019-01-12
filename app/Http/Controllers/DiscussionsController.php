95c6b34ea6934856eeaba3fc4cec6c3ea825066f
MaxDrljic <max_spree@hotmail.com>
2019-01-12 09:38:03 +0100

    Storing discussions & some minor code refactoring

--- app/Http/Controllers/DiscussionsController.php
+++ app/Http/Controllers/DiscussionsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\Discussion;
 use Illuminate\Http\Request;
 
 class DiscussionsController extends Controller
@@ -13,6 +15,28 @@ class DiscussionsController extends Controller
 
     public function store()
     {
-        dd(request());
+        $r = request();
+        $this->validate($r, [
+            'channel_id'   => 'required',
+            'content'      => 'required',
+            'title'        => 'required'
+        ]);
+
+        $discussion = Discussion::create([
+            'title'         => $r->title,
+            'content'       => $r->content,
+            'channel_id'    => $r->channel_id,
+            'user_id'       => Auth::id(),
+            'slug'          => str_slug($r->title)
+        ]);
+
+        toastr()->success('Discussion successfully created!');
+
+        return redirect()->route('discussion', ['slug' => $discussion->slug]);
+    }
+
+    public function show($slug)
+    {
+        return view('discussions.show')->with('discussion', Discussion::where('slug', $slug)->first());
     }
 }
