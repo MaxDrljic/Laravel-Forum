f669fa9b4b141fb184b96e3569ea2a6a209f5ddb
MaxDrljic <max_spree@hotmail.com>
2019-01-12 09:04:28 +0100

    Discussions page

--- resources/views/discuss.blade.php
+++ resources/views/discuss.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="card">
+        <div class="card-header text-center">Create a new discussion</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="card-body">
+            <form action="{{ route('discussions.store') }}" method="POST">
+                {{ csrf_field() }}
 
-                    You are logged in!
+                <div class="form-group">
+                    <label for="channel">Pick a channel</label>
+                    <select name="channel_id" id="channel_id" class="form-control">
+                        @foreach($channels as $channel)
+                            <option value="{{ $channel->id }}">{{ $channel->title }}</option>
+                        @endforeach
+                    </select>
                 </div>
-            </div>
+                <div class="form-group">
+                    <label for="content">Ask a question</label>
+                    <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success pull-right" type="submit">Create discussion</button>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,6 +84,10 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-4">
+
+                        <a href="{{ route('discussions.create') }}" class="form-control btn btn-primary">Create a new discussion</a>
+                        <br><br>
+
                         <div class="card">
                             <div class="card-header">Channels</div>
                             <div class="card-body">
